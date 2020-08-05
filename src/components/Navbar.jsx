077e9b6da9ed2a91de0e0e7565f7b29137df714f
Alex Williams <alexjwilliams1@gmail.com>
2020-08-05 15:44:47 -0700

    mvp complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 import { useDarkMode } from '../hooks/useDarkMode'
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+  const {
+    darkMode,
+    setDarkMode
+  } = props
+
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
 import {useLocalStorage} from './useLocalStorage'
 
 export const useDarkMode = (key) => {
-    const[darkMode, setDarkMode] = useLocalStorage(key)
+    const[darkMode, setDarkMode] = useLocalStorage('darkMode')
 
     return [darkMode, setDarkMode]
 }
